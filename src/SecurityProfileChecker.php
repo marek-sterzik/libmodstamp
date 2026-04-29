4340966df671f89ef573c58874ce1cabe4a52045
Marek Sterzik <marek.sterzik@gmail.com>
2026-04-29 19:15:47 +0200

    implement security profile checker

--- src/SecurityProfileChecker.php
+++ src/SecurityProfileChecker.php
@@ -8,6 +8,70 @@ class SecurityProfileChecker
 {
     public static function checkSecurityProfile(array $config): array
     {
+        if (!$this->checkOrdinaryArray($config)) {
+            $config = $config['profiles'] ?? [];
+            if (!$this->checkOrdinaryArray($array)) {
+                throw new Exception("profile list must be an array");
+            }
+        }
+        foreach ($config as $id => &$profile) {
+            $this->checkProfile($id, $profile);
+        }
         return $config;
+    }
+
+    private function checkProfile(int $id, array &$profile): void
+    {
+        if (!isset($profile['encryption'])) {
+            throw new Exception(sprintf("missing encryption in profile %d", $id));
+        }
+        if (!is_string($profile['encryption'])) {
+            throw new Exception(sprintf("invalid encryption in profile %d", $id));
+        }
+        if (isset($profile['permission'])) {
+            if (!is_string($profile['permission'])) {
+                throw new Exception(sprintf("invalid permission set in profile %d", $id));
+            }
+            $profile['permission'] = strtolower($profile['permission']);
+            if (!in_array($profile['permission'], ['none', 'ro', 'rw', 'default'])) {
+                throw new Exception(sprintf("invalid permission set in profile %d", $id));
+            }
+            if ($profile['permission'] === 'default') {
+                unset($profile['permission']);
+            }
+        } else {
+            unset($profile['permission']);
+        }
+        if (isset($profile['id'])) {
+            if (!is_string($profile['id'])) {
+                throw new Exception(sprintf("invalid profile identifier in profile %d", $id));
+            }
+        } else {
+            unset($profile['id']);
+        }
+        if (isset($profile['hosts'])) {
+            if (!is_array($profile['hosts'])) {
+                $profile['hosts'] = [$profile['hosts']];
+            }
+            if (!$this->checkOrdinaryArray($profile['hosts'])) {
+                throw new Exception(sprintf("invalid profile hosts in profile %d: array required", $id));
+            }
+            foreach ($profile['hosts'] as &$host) {
+                if (!is_string($host)) {
+                    throw new Exception(sprintf("invalid profile hosts in profile %d: host is not a string", $id));
+                }
+            }
+        } else {
+            unset($profile['hosts']);
+        }
+    }
+
+    private function checkOrdinaryArray(array $array): bool
+    {
+        $keys = array_keys($array);
+        if ($keys === array_keys($keys)) {
+            return true;
+        }
+        return false;
     }
 }
